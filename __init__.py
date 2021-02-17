7b4e1d6ee1c9d1b307653dab0b4f9cc27ef56e07
Antonio González <antgon@cantab.net>
2021-02-17 13:57:59 +0000

    Removed init file

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .msn import cell
-from .msn import instrumentation
-from .msn import modulation
-
-from .msn.cell import MSN
-#from .msn.cell import synaptic_input
-#from .msn.instrumentation import (Stim, ActionPotentials)
-#from .msn.modulation import (Dopamine, Acetylcholine)
